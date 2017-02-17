9788bee7df3dcad0f06c02a35811f9f392cab889
Administrator <516893232@qq.com>
2017-02-17 18:11:53 +0800

    提交"我"界面

---
+++ app/src/main/java/com/qf/pearvideo/activity/MineActivity.java
@@ -0,0 +1,15 @@
+package com.qf.pearvideo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qf.pearvideo.R;
+
+public class MineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
