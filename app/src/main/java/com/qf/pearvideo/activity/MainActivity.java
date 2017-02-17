88dc77203fa1d93f928f161944ac2c38c2cf8408
Administrator <516893232@qq.com>
2017-02-17 15:45:17 +0800

    分包

--- app/src/main/java/com/qf/pearvideo/activity/MainActivity.java
+++ app/src/main/java/com/qf/pearvideo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.qf.pearvideo;
+package com.qf.pearvideo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.qf.pearvideo.R;
 
 public class MainActivity extends AppCompatActivity {
 
