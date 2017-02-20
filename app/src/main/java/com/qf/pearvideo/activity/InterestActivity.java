2ce044a0f8c4e35fbd5730b1c94383976a6c9085
PearVideo <anddl9395@163.com>
2017-02-20 11:57:57 +0800

    兴趣页面

---
+++ app/src/main/java/com/qf/pearvideo/activity/InterestActivity.java
@@ -0,0 +1,20 @@
+package com.qf.pearvideo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qf.pearvideo.R;
+
+/**
+ * 兴趣页面
+ * 2017/2/20
+ * dengliang
+ */
+public class InterestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interest);
+    }
+}
--- app/src/main/java/com/qf/pearvideo/fragment/Dengliang.java
+++
@@ -1,8 +0,0 @@
-package com.qf.pearvideo.fragment;
-
-/**
- * Created by Administrator on 2017/2/17.
- */
-
-public class Dengliang {
-}
